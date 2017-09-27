acfd558d0497853e053abf485a6e0cadd1718c39
Adam Szlachta <adam.szlachta@gmail.com>
2017-09-27 02:34:02 +0200

    Datatype for particle

--- src/PSO.hs
+++ src/PSO.hs
@@ -16,11 +16,37 @@ import           Config (Config())
 import qualified Config
 import qualified Functions
 
-newtype Particle = Particle { position  :: VU.Vector Double  } deriving (Show)
-newtype Swarm    = Swarm    { particles :: V.Vector Particle } deriving (Show)
+newtype Position = Position
+    { unPos :: VU.Vector Double
+    } deriving (Show)
+
+newtype Velocity = Velocity
+    { unVel :: VU.Vector Double
+    } deriving (Show)
+
+newtype Value = Value
+    { unVal :: Double
+    } deriving (Show)
+
+data Particle = Particle
+    { position           :: Position
+    , velocity           :: Velocity
+    , bestPosition       :: Position
+    , bestValue          :: Value
+    , bestGlobalPosition :: Position
+    , bestGlobalValue    :: Value
+    } deriving (Show)
+
+newtype Swarm = Swarm
+    { particles :: V.Vector Particle
+    } deriving (Show)
 
 genParticle :: (PrimMonad m, MonadPrim m) => Int -> Double -> Double -> Rand m Particle
-genParticle dim from to = fmap Particle $ VU.replicateM dim $ RM.uniformR (from, to)
+genParticle dim from to = do
+    position <- fmap Position $ VU.replicateM dim $ RM.uniformR (from, to)
+    velocity <- fmap Velocity $ VU.replicateM dim $ RM.uniformR (from, to)
+    let bestValue = Value 10000.0
+    return $ Particle position velocity position bestValue position bestValue
 
 genSwarm :: (PrimMonad m, MonadPrim m) => Int -> Int -> Double -> Double -> Rand m Swarm
 genSwarm n dim from to = fmap Swarm $ V.replicateM n $ genParticle dim from to
